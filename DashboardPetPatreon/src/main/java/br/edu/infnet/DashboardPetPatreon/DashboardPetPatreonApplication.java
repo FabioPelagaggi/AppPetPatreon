616fcfc4f2346ecd1a3aa9bd2ac5f4ff708f4def
Fabio Pelagaggi <fabio.pnunes@al.infnet.edu.br>
2023-03-08 21:19:19 -0300

    Update Feature 5

---
+++ DashboardPetPatreon/src/main/java/br/edu/infnet/DashboardPetPatreon/DashboardPetPatreonApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.DashboardPetPatreon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardPetPatreonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardPetPatreonApplication.class, args);
+	}
+
+}
---
+++ DashboardPetPatreon/src/test/java/br/edu/infnet/DashboardPetPatreon/DashboardPetPatreonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.DashboardPetPatreon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardPetPatreonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
